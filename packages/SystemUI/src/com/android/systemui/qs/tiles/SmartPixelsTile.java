13a0461a0f15f69eb1bbe14228f2da7e850c7430
Mudit200408 <muditpurohit2004.pm@gmail.com>
2023-09-04 10:55:09 +0530

    misc

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SmartPixelsTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SmartPixelsTile.java
@@ -158,7 +158,7 @@ public class SmartPixelsTile extends QSTileImpl<BooleanState> implements
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.SUPERIOR;
+        return MetricsEvent.CUSTOM;
     }
 
     @Override
